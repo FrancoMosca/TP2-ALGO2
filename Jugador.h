d7136e9c265623c7c336ad1328c208d8a44334e1
cassandrag3 <131493924+cassandrag3@users.noreply.github.com>
2023-06-14 16:11:05 -0300

    Update Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -21,24 +21,33 @@ private:
     int cantidadInsertsRestantes;
 
 public:
+    //Pre: -
+    //Post: se crea el jugador de nombre " nombre " y identiificador "simboloDeFicha"
     Jugador(string nombre, char simboloDeFicha);
-
+    //Pre: -
+    //Post: retorna un entero que representa el numero de identificacion asociado al jugador
     int getIdJugador();
-
+    //Pre: - 
+    //Post: el jugador queda asociado al identificador "idJugador"
     void setIdJugador(int idJugador);
-
+    //Pre: -
+    //Post: retorna el nombre asociado al jugador
     const string &getNombreJugador();
-
+    //Pre: - 
+    //Post: retorna un puntero a la ficha asociada al jugador
     Ficha *getFicha();
-
+    //Pre: - 
+    //Post: reorna un puntero al mazo del jugador
     Mazo *getMazo() const;
-
+    //Pre: -
+    //Post: el jugador tiene un mazo "mazo" de cartas
     void setMazo(Mazo *mazo);
-
+    //Pre: -
+    //Post: retorna un entero que representa la cantidad de elementos restantes permitidos que se pueden colocar en el tablero
     int getCantidadInsertsRestantes();
-
+    //Pre: -
+    //Post: el jugador tiene "cantidadInsertsRestantes" cantidad de elementos restantes permitidos que se pueden colocar en el tablero
     void setCantidadInsertsRestantes(int cantidadInsertsRestantes);
-
 };
 
 

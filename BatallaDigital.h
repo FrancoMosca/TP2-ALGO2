648e0ef4e9e6123aaff8780cf533412a42a0b5d9
cassandrag3 <131493924+cassandrag3@users.noreply.github.com>
2023-06-14 15:02:47 -0300

    Update BatallaDigital.h

--- BatallaDigital.h
+++ BatallaDigital.h
@@ -21,59 +21,114 @@ private:
     int cantidadJugadasRealizadas;
     int cantidadInsertsPorJugador;
 public:
+    //Pre: -
+    //Post: Crea una batalla no inicializada 
     BatallaDigital();
+    //Pre: -
+    //Post: se libera la memoria solicitada 
     ~BatallaDigital();
+    //Pre: -
+    //Post: Se inicializan todas las entidades del juego (tablero, jugadores, fichas, mazo de cartas)
     void iniciarJuego();
+    //Pre: -
+    //Post: se solicitan por pantalla las dimensiones del tablero y se crea el tablero donde se realizara el juego
     void crearTableroPrincipal();
+    //Pre: -
+    //Post: se solicitan los datos de cada jugador presente 
     void crearJugadores();
+    //Pre:
+    //Post:
     bool esFichaOcupadaPorOtroJugador(char simboloFichaElegido);
+    //Pre:
+    //Post:
     void imprimirJugadores();
+    //Pre:
+    //Post:
     void mostrarTableroPorCoordenadas();
+    //Pre:
+    //Post:
     int getCantidadJugadores();
+    //Pre:
+    //Post:
     void crearMazo();
+    //Pre:
+    //Post:
     void crearMazoPorJugador();
     // Funciones para iniciar el juego
+    //Pre:
+    //Post:
     void jugarJuego();
+    //Pre:
+    //Post:
     void solicitarIngresoDeCordenadas(int &filas, int &columnas, int &profundidad);
+    //Pre:
+    //Post:
     bool esFichaValida(int &fila, int &columna, int &profundidad);
+    //Pre:
+    //Post:
     bool estaEnRangoValido(int &fila, int &columna, int &profundidad);
+    //Pre:
+    //Post:
     bool estaCasilleroLibre(int &fila, int &columna, int &profundidad);
+    //Pre:
+    //Post:
     void repartirCartasAlJugadorActual();
+    //Pre:
+    //Post:
     void iniciarTurnos();
+    //Pre:
+    //Post:
     void usarCarta(int numeroCarta);
-
-
+    //Pre:
+    //Post:
     void aplicarHabilidadCarta(Carta *carta);
-
+    //Pre:
+    //Post:
     void crearArmamentoDelJugador();
-
+    //Pre:
+    //Post:
     void obtenerCantidadDeInsertsPorJugador();
+    //Pre:
+    //Post:
     void generarPosiciones(int cantidadElementos, char simboloFicha, Jugador *jugador);
-
+    //Pre:
+    //Post:
     bool validarInsertsDisponibles(int cantidadElementos, int insertsDisponibles);
-
+    //Pre:
+    //Post:
     bool esFichaValidaMina(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     void agregarMina(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     void elegirCarta();
-
+    //Pre:
+    //Post:
     void repartirCartas();
-
+    //Pre:
+    //Post:
     void decidirMoverSoldadoArmamento(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     bool validarMovimiento(char movimiento);
-
+    //Pre:
+    //Post:
     void moverElemento(int fila, int columna, int profundidad, char movimiento, int nuevaProfundidad);
-
+    //Pre:
+    //Post:
     bool validarSoldadoArmamentoElegido(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     bool existeMina(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     void eliminarSoldado(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     bool existeSoldadoEnemigo(int fila, int columna, int profundidad);
-
+    //Pre:
+    //Post:
     bool esPosicionBloqueada(int fila, int columna, int profundidad);
 };
 

f8ea7f2237c1736a6a3e73b7dedf73e62066f9f0
cassandrag3 <131493924+cassandrag3@users.noreply.github.com>
2023-06-14 02:04:04 -0300

    Update Casillero.h

--- Casillero.h
+++ Casillero.h
@@ -17,8 +17,14 @@ private:
     terreno_t terreno;
 
 public:
+    //Pre: -
+    //Post: se crea el casillero cuya ficha ubicada en el es "simboloFicha" y es del tipo "terreno" 
     Casillero(terreno_t terreno, char simboloFicha);
+    //Pre: -
+    //Post: retorna el puntero a la clase Ficha asociado al caillero 
     Ficha * getFicha();
+    //Pre: -
+    //Post: retorna el valor booleano True si la ficha asociada al casillero se encuentra vacia y False en caso contrario
     bool estaCasilleroVacio();
 };
 

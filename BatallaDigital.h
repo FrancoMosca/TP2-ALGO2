dfed6fdaffc713cc06c558ee62ed481249628a21
cassandrag3 <131493924+cassandrag3@users.noreply.github.com>
2023-06-14 15:58:40 -0300

    Update BatallaDigital.h

--- BatallaDigital.h
+++ BatallaDigital.h
@@ -36,32 +36,32 @@ public:
     //Pre: -
     //Post: se solicitan los datos de cada jugador presente 
     void crearJugadores();
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: retorna el valor boolenao True en caso de que ya exista un jugador con el identificador "simboloFichaElegido", en caso cotrario retorna False 
     bool esFichaOcupadaPorOtroJugador(char simboloFichaElegido);
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: se muestren por pantalla los jugadores presentes en la partida 
     void imprimirJugadores();
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: se muestra el estado del tablero en formato txt
     void mostrarTableroPorCoordenadas();
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: retorna un entero que reprenta la cantidad de jugadores totales en el juego
     int getCantidadJugadores();
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: se crea un mazo donde se encotraran todas las cartas disponibles para usar durante la partida 
     void crearMazo();
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: se crea un mazo por cada jugador en el que se encontraran las cartas disponibles para cada uno
     void crearMazoPorJugador();
     // Funciones para iniciar el juego
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: se realiza una ronda completa de jugadas
     void jugarJuego();
-    //Pre:
-    //Post:
+    //Pre: -
+    //Post: se solicita por pantalla la eleccion de numero de fila, columna y profundidad, su valor permanecera guardado en "filas","columnas","profundidad"  
     void solicitarIngresoDeCordenadas(int &filas, int &columnas, int &profundidad);
-    //Pre:
+    //Pre:recibe como parametro tres enteros que representan 
     //Post:
     bool esFichaValida(int &fila, int &columna, int &profundidad);
     //Pre:

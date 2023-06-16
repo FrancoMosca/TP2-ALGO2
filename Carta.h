a431881ffb2ebf6bc3c5365b0c7296d539724757
Josellr94 <128191655+Josellr94@users.noreply.github.com>
2023-06-15 22:09:45 -0300

    Carta.cpp y Carta.h terminados con pre y post

--- Carta.h
+++ Carta.h
@@ -20,88 +20,83 @@ private:
 
 public:
     //Pre:
-    //Post:
+    //Post: Crea una carta con habilidad sin efecto.
     Carta();
 
     //Pre:
-    //Post:
+    //Post: Crea una carta con habilidad el efecto dado.
     Carta(habilidadCarta_t efectoCarta);
 
     //Pre:
-    //Post:
+    //Post: Setea una habilidad del enum.
     void setHabilidad(habilidadCarta_t numero);
 
     //Pre:
-    //Post:
+    //Post: Retorna la habilidad de la carta.
     habilidadCarta_t getHabilidad();
 
     //Pre:
-    //Post:
+    //Post: Devuelve una habilidad random de las que estan en el enum.
     habilidadCarta_t generarEfectoAleatorio();
 
     //Pre:
-    //Post:
+    //Post: Imprime por consola el nombre de la habilidad de la carta.
     void imprimirHabilidadCarta();
 
     //Pre:
-    //Post:
+    //Post: Se destruye la carta.
     virtual ~Carta();
 
-    //Pre:
-    //Post:
-    //Metodos carta avion
+    //Pre: El jugador tiene que poseer un avion en el tablero.
+    //Post: Activa la carta Radar que obtiene y muestra por pantalla las minas de los enemigos.
     void avionRadar(Tablero *tablero, int idJugador);
 
-    //Pre:
-    //Post:
-    //Metodos carta barco
+    //Pre: El jugador tienen que poseer un barco en el tablero.
+    //Post: Activa la carta disparar Misil. Dispara y destruye la ficha en la coordenada dada por el jugador y bloquea la casilla por 3 turnos.
     void dispararMisil(Tablero *tablero, int idJugador);
 
     //Pre:
-    //Post:
-    //Metodos ataque quimico
+    //Post: Activa la carta ataque Quimica la cual contamina y destruye la ficha en la coordenada indicada bloqueando por 10 turnos el centro, por 8 las casillas adyacentes, y por 6 las
+    //      mas alejadas del centro. Teniendo un rango de 5x5x5. 125 casillas.
     void ataqueQuimico(Tablero *tablero);
 
-    //Pre:
-    //Post:
-    //Metodos avion Kamikaze
+    //Pre: El jugador tiene que poseer un avion en el tablero y deben haber barcos enemigos.
+    //Post: Se activa la carta avion Kamikaze. Que destruye un avion aliado para destruir un barco enemigo aleatorio.
     void avionKamikaze(Tablero *tablero, int idJugador);
 
-
-
+    //Pre: Solo se puede usar en las casillas que son de terreno AIRE.
+    //Post: Se activa la carta tormenta electrica. Que genera un tornado de 5x5x5, en cualquier casilla que sea de Aire y detruye todos los aviones en ella.
     void tormentaElectrica(Tablero *tablero);
-    //Pre:
-    //Post:
-    //Metodos escudo
-
+    //Pre: El jugador tiene que poseer un soldado en el tablero.
+    //Post: Se activa la carta francotirador. Que le muestra al jugador un listado de los soldados enemigos, el cual podra eliminar a uno de ellos.
     void francotirador(Tablero *tablero, int idJugador);
 
     //Pre:
-    //Post:
+    //Post: Solicita al usuario el ingreso de coordenadas. Fila, columna y profundidad.
     void solicitarIngresoDeCordenadas(int &filas, int &columnas, int &profundidad);
 
     //Pre:
-    //Post:
+    //Post: Valida que las coordenadas dadas no esten fuera de rango.
     bool estaEnRangoValido(Tablero *tablero, int &fila, int &columna, int &profundidad);
 
     //Pre:
-    //Post:
+    //Post: Valida que las coordenadas dadas no esten fuera del tablero y le indica al usuario.
     bool esFichaValida(Tablero *tablero,int &fila, int &columna, int &profundidad);
 
     //Pre:
-    //Post:
+    //Post: Valida que las coordenadas dadas no esten fuera del tablero.
     bool estaDentroDeTablero(Tablero *tablero,int &fila, int &columna, int &profundidad);
     //Pre:
-    //Post:
+    //Post: Valida si el jugador pasado posee un avion en el tablero.
     bool poseeAvion(Tablero *tablero, int idJugador);
     //Pre:
-    //Post:
+    //Post: Valida si el jugador pasado posee un barco en el tablero.
     bool poseeBarco(Tablero *tablero, int idJugador);
     //Pre:
-    //Post:
+    //Post: Valida si el jugador pasado posee un soldado en el tablero.
     bool poseeSoldado(Tablero *tablero, int idJugador);
     //Pre:
-    //Post:
+    //Post: Valida que las coordenadas dadas sea un casillero de AIRE.
     bool estaEnElAire(Tablero *tablero, int &fila, int &columna, int &profundidad);
 
 };

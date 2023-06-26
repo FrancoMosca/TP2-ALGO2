6189bb52e1fe7090a3c43eb49821e74f9adc01e7
cassandrag3 <131493924+cassandrag3@users.noreply.github.com>
2023-06-26 02:09:50 -0300

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -1,30 +1,50 @@
-#ifndef INTERFAZ_H_
-#define INTERFAZ_H_
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
 #include "EasyBMP.h"
 #include "Tablero.h"
 
+//Pre: recibe un caracter "inicial" que repesenta el color que se quiere pintar y un entero que representa la oscuridad del color (1 o 2)
+//Post: retorna un tipo de dato RGBApixel que representa un color
+RGBApixel crearColor(char inicial, int numero);
+
 //Pre: -
-//Post: dentro de la imagen "ubicacion" se pinta de "color" la celda representada por la posicion "fila" x "columna" de un tablero cuyas dimensiones son "totalfilas" x "totalColumnas"
-void colorSector(int fila, int columna,RGBApixel color,BMP* ubicacion,int totalFilas, int totalColumnas);
+//Post: genera la imagen de un soldado
+void agregarSoldado(int fila, int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
 //Pre: -
-//Post: dentro de la imagen "ubicacion" se colorea de tal forma que se distinga una S en la celda representada por la posicion "fila" x "columna" de un tablero cuyas dimensiones son "totalfilas" x "totalColumnas"
-void agregarSoldado(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
-//Pre: -
-//Post: dentro de la imagen "ubicacion" se colorea de tal forma que se distinga una M en la celda representada por la posicion "fila" x "columna" de un tablero cuyas dimensiones son "totalfilas" x "totalColumnas"
+//Post: genera la imagen de una mina
 void agregarMina(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
 //Pre: -
-//Post: dentro de la imagen "ubicacion" se colorea de tal forma que se distinga una B en la celda representada por la posicion "fila" x "columna" de un tablero cuyas dimensiones son "totalfilas" x "totalColumnas"
+//Post: genera la imagen de un barco
 void agregarBarco(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
 //Pre: -
-//Post: dentro de la imagen "ubicacion" se colorea de tal forma que se distinga una A en la celda representada por la posicion "fila" x "columna" de un tablero cuyas dimensiones son "totalfilas" x "totalColumnas"
+//Post: genera la imagen de un casillero inactivo
+void colorSector(int fila, int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
+//Pre: -
+//Post: genera la imagen de un avion
 void agregarAvion(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
 //Pre: -
-//Post: se colorea la imagen "ubicacion" buscando que se grafique un tablero cuyas dimensiones sean "totalfilas" x "totalColumnas"
-void crearTabla(BMP* ubicacion,int totalFilas, int totalColumnas);
-//Pre: recibe como parametro el tablero que se busca representar junto con la ubicacion donse se representara , el nivel de profudidad que se quiere repesentar y el jugador al que se lo quiere mostrar
-//Post: se colorea la imagen "ubicacion" de tal forma que sean visibles las fichas ubicadas en el nievel "altura" de profundidad que estan asociadas al jugador "jugador"
-void mostrarCapa(Tablero* tablero, int altura, BMP* ubicacion, int jugador);
-//Pre: recibe como parametro el tablero que se busca representar  y el jugador al que se lo quiere mostrar
-//Post: se crean 6 imagenes en formato .BNP donde cada una de ellas representara un nivel de profundidad diferente
+//Post: genera la imagen de un casillero de tierra
+void agregarTierra(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
+//Pre: -
+//Post: genera la imagen de un casillero de agua
+void agregarAgua(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
+//Pre: -
+//Post: genera la imagen de un casillero de cielo
+void agregarCielo(int fila,int columna,BMP* ubicacion,int totalFilas, int totalColumnas);
+
+//Pre: -
+//Post: se genera la imagen para el tablero en la profundidad "altura"
+void mostrarCapa(Tablero *tablero, int altura, BMP* ubicacion, int jugador);
+
+//Pre: -
+//Post: se genera una imagen por piso del tablero
 void mostrarTablero(Tablero* tablero, int jugador);
-#endif /* INTERFAZ_H_ */
+
+#endif /* INTERFACE_H_ */
